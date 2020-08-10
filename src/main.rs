7124e00d288b46ef43379f33cd7321ebfec09b85
Imed Adel <aadelimed@gmail.com>
2020-08-10 14:11:58 +0000

    Use `decode_config_slice` instead of `decode` from `base64` crate

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,26 @@
-use base64::{decode, encode};
+use base64::{decode_config_slice, encode};
 use dotenv;
 use rand::Rng;
 
 fn main() {
     let key = "FOO";
     let value = dotenv::var(key).unwrap(); // Loads env variable. Not used in this code though.
+
     let private_key = rand::thread_rng().gen::<[u8; 32]>(); // generates 32 bytes array
     let encoded = encode(&private_key); // converts array to base64
-    let dec = decode(encoded.clone()).unwrap(); // converts base64 to vector.
-    let decoded = {
-        let mut array = [0u8; 32];
-        for (&x, p) in dec.as_slice().iter().zip(array.iter_mut()) {
-            *p = x;
-        }
-        array
-    }; // converts vector to 32 bytes array. pretty sure there is an easier way of doing it. but this already works.
+    let mut decoded = [0u8; 32]; // initializes an 32 bytes array
+    decode_config_slice(encoded.clone(), base64::STANDARD, &mut decoded).unwrap(); // nb. encode is a shorthand for encode_config with base64::STANDARD (see docs)
     println!(
         "Hello, world!,
         Private Key:\t{:?},
         Length:\t{},
         FOO:\t{},
         Base64:\t{},
-        Decoded:\t{:?}",
+        Decoded:\t{:?},",
         private_key,
         private_key.len(),
         value,
         encoded,
-        decoded
+        decoded,
     );
 }
