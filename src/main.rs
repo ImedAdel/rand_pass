821f46a2775419f8a6ca2e8f969d8260c5c0eaeb
Imed Adel <aadelimed@gmail.com>
2020-08-10 13:51:44 +0000

    Comment each line for future reference

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,17 @@ use rand::Rng;
 
 fn main() {
     let key = "FOO";
-    let value = dotenv::var(key).unwrap();
-    let private_key = rand::thread_rng().gen::<[u8; 32]>();
-    let encoded = encode(&private_key);
-    let dec = decode(encoded.clone()).unwrap();
+    let value = dotenv::var(key).unwrap(); // Loads env variable. Not used in this code though.
+    let private_key = rand::thread_rng().gen::<[u8; 32]>(); // generates 32 bytes array
+    let encoded = encode(&private_key); // converts array to base64
+    let dec = decode(encoded.clone()).unwrap(); // converts base64 to vector.
     let decoded = {
         let mut array = [0u8; 32];
         for (&x, p) in dec.as_slice().iter().zip(array.iter_mut()) {
             *p = x;
         }
         array
-    };
+    }; // converts vector to 32 bytes array. pretty sure there is an easier way of doing it. but this already works.
     println!(
         "Hello, world!,
         Private Key:\t{:?},
